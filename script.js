da7d284beaa36a0f388c04b5d47d972c7fdc15a7
Ralph Bankston <ralph@Ralphs-MacBook-Air.local>
2022-07-06 22:21:51 -0400

    Refresher

---
+++ script.js
@@ -0,0 +1,17 @@
+let firstName = "Ralph";
+let lastName = "Bankston";
+let fullName =  firstName + " " + lastName;
+console.log(fullName);
+let population = 48437;
+console.log(population);
+console.log(population*3);
+let javascriptIsCool = true;
+let colors = ["violet", "indigo", "blue", "green", "yellow", "orange", "red"];
+
+for (let i = 0; i < 5; i++) {
+    console.log(i+1);
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
